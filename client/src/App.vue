2271f305bbaf604b4523ce0a8b34464b8fdc9dae
Mark Anikin <mark.corray.off@gmail.com>
2023-03-29 17:35:43 +0300

    Первый коммит

---
+++ client/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <main>
+        <header>
+            <div class="logo">
+                <img src="./components/media/i-logo.png" alt="">
+                <h3>База данных</h3>
+            </div>
+        </header>
+        <div class="content">
+            <div class="content-container">
+
+            </div>
+            <div class="sidebar">
+                <ul>
+                    <li><button>Список пользователей</button></li>
+                    <li><button>Новый пользователь</button></li>
+                    <li><button>Удалить по параметру</button></li>
+                    <li><button>Изменить по параметру</button></li>
+                    <li><button>Отправить e-mail</button></li>
+                    <li><button>Отсортировать</button></li>
+                    <li><button>Сохранить изменения</button></li>
+                    <li><button>Выход</button></li>
+                </ul>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped></style>
